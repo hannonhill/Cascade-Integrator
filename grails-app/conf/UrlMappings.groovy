3f47b9945489435d22ecf00c40f47941e09c5ecc
David Klanac <david.klanac@hannonhill.com>
2010-09-02 17:25:09 -0400

    Added some more todo tasks and set the Site:list action as the application start screen

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,7 +7,7 @@ class UrlMappings {
 			}
 		}
 
-		"/"(view:"/index")
+		"/"(controller:"site", action:"list")
 		"500"(view:'/error')
 	}
 }
--- grails-app/controllers/com/hannonhill/integrator/SiteController.groovy
+++ grails-app/controllers/com/hannonhill/integrator/SiteController.groovy
@@ -6,110 +6,115 @@ class SiteController {
 	
 	def scaffold = true
 	
-    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
-
-    def index = {
-        redirect(action: "list", params: params)
-    }
-
-    def list = {
-        params.max = Math.min(params.max ? params.int('max') : 10, 100)
-        [siteInstanceList: Site.list(params), siteInstanceTotal: Site.count()]
-    }
-
-    def create = {
-        def siteInstance = new Site()
-        siteInstance.properties = params
-        return [siteInstance: siteInstance]
-    }
-
-    def save = {
-        def siteInstance = new Site(params)
+	static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+	
+	def index = {
+		redirect(action: "list", params: params)
+	}
+	
+	def list = {
+		params.max = Math.min(params.max ? params.int('max') : 10, 100)
+		[siteInstanceList: Site.list(params), siteInstanceTotal: Site.count()]
+	}
+	
+	def create = {
+		def siteInstance = new Site()
+		siteInstance.properties = params
+		return [siteInstance: siteInstance]
+	}
+	
+	def save = {
+		def siteInstance = new Site(params)
 		def homepageInstance = new ContentType(name: "Homepage")
 		def standardInstance = new ContentType(name: "Standard")
 		siteInstance.addToContentTypes(homepageInstance)
 		siteInstance.addToContentTypes(standardInstance)
 		
-		//create site, templates, page configurations, config sets, content types, homepage and standard page base asset
+		//create site and content type including all related content type objects
 		//TODO Add try...catch block to handle timeouts and connection exceptions		
 		CascadeSite site = siteInstance.createRemoteSite()
 		homepageInstance.createRemoteContentType(site)
 		standardInstance.createRemoteContentType(site)
-				
-        if (siteInstance.save(flush: true)) {
-            flash.message = "${message(code: 'default.created.message', args: [message(code: 'site.label', default: 'Site'), siteInstance.id])}"
-            redirect(action: "show", id: siteInstance.id)
-        }
-        else {
-            render(view: "create", model: [siteInstance: siteInstance])
-        }
-    }
-
-    def show = {
-        def siteInstance = Site.get(params.id)
-        if (!siteInstance) {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'site.label', default: 'Site'), params.id])}"
-            redirect(action: "list")
-        }
-        else {
-            [siteInstance: siteInstance]
-        }
-    }
-
-    def edit = {
-        def siteInstance = Site.get(params.id)
-        if (!siteInstance) {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'site.label', default: 'Site'), params.id])}"
-            redirect(action: "list")
-        }
-        else {
-            return [siteInstance: siteInstance]
-        }
-    }
-
-    def update = {
-        def siteInstance = Site.get(params.id)
-        if (siteInstance) {
-            if (params.version) {
-                def version = params.version.toLong()
-                if (siteInstance.version > version) {
-                    
-                    siteInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'site.label', default: 'Site')] as Object[], "Another user has updated this Site while you were editing")
-                    render(view: "edit", model: [siteInstance: siteInstance])
-                    return
-                }
-            }
-            siteInstance.properties = params
-            if (!siteInstance.hasErrors() && siteInstance.save(flush: true)) {
-                flash.message = "${message(code: 'default.updated.message', args: [message(code: 'site.label', default: 'Site'), siteInstance.id])}"
-                redirect(action: "show", id: siteInstance.id)
-            }
-            else {
-                render(view: "edit", model: [siteInstance: siteInstance])
-            }
-        }
-        else {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'site.label', default: 'Site'), params.id])}"
-            redirect(action: "list")
-        }
-    }
-
-    def delete = {
-        def siteInstance = Site.get(params.id)
-        if (siteInstance) {
-            try {
-                siteInstance.delete(flush: true)
-                flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'site.label', default: 'Site'), params.id])}"
-                redirect(action: "list")
-            }
-            catch (org.springframework.dao.DataIntegrityViolationException e) {
-                flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'site.label', default: 'Site'), params.id])}"
-                redirect(action: "show", id: params.id)
-            }
-        }
-        else {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'site.label', default: 'Site'), params.id])}"
-            redirect(action: "list")
-        }
-    }
+		
+		//		TODO create CMS Content Type objects in ContentTypeService
+		//		TODO create _internal folder tree
+		//		TODO create templates in _internal/templates folder
+		//		TODO create Scaffold domain type for folders, index pages
+		
+		if (siteInstance.save(flush: true)) {
+			flash.message = "${message(code: 'default.created.message', args: [message(code: 'site.label', default: 'Site'), siteInstance.id])}"
+			redirect(action: "show", id: siteInstance.id)
+		}
+		else {
+			render(view: "create", model: [siteInstance: siteInstance])
+		}
+	}
+	
+	def show = {
+		def siteInstance = Site.get(params.id)
+		if (!siteInstance) {
+			flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'site.label', default: 'Site'), params.id])}"
+			redirect(action: "list")
+		}
+		else {
+			[siteInstance: siteInstance]
+		}
+	}
+	
+	def edit = {
+		def siteInstance = Site.get(params.id)
+		if (!siteInstance) {
+			flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'site.label', default: 'Site'), params.id])}"
+			redirect(action: "list")
+		}
+		else {
+			return [siteInstance: siteInstance]
+		}
+	}
+	
+	def update = {
+		def siteInstance = Site.get(params.id)
+		if (siteInstance) {
+			if (params.version) {
+				def version = params.version.toLong()
+				if (siteInstance.version > version) {
+					
+					siteInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'site.label', default: 'Site')] as Object[], "Another user has updated this Site while you were editing")
+					render(view: "edit", model: [siteInstance: siteInstance])
+					return
+				}
+			}
+			siteInstance.properties = params
+			if (!siteInstance.hasErrors() && siteInstance.save(flush: true)) {
+				flash.message = "${message(code: 'default.updated.message', args: [message(code: 'site.label', default: 'Site'), siteInstance.id])}"
+				redirect(action: "show", id: siteInstance.id)
+			}
+			else {
+				render(view: "edit", model: [siteInstance: siteInstance])
+			}
+		}
+		else {
+			flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'site.label', default: 'Site'), params.id])}"
+			redirect(action: "list")
+		}
+	}
+	
+	def delete = {
+		def siteInstance = Site.get(params.id)
+		if (siteInstance) {
+			try {
+				siteInstance.delete(flush: true)
+				flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'site.label', default: 'Site'), params.id])}"
+				redirect(action: "list")
+			}
+			catch (org.springframework.dao.DataIntegrityViolationException e) {
+				flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'site.label', default: 'Site'), params.id])}"
+				redirect(action: "show", id: params.id)
+			}
+		}
+		else {
+			flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'site.label', default: 'Site'), params.id])}"
+			redirect(action: "list")
+		}
+	}
 }
